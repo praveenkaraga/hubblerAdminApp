a8bae1b19246176753095f57db6490f65cc1cdba
anushachengi <anusha@hubbler.mobi>
2019-12-02 18:58:51 +0530

    Merge branch 'user_console_v2' of https://github.com/praveenkaraga/hubblerAdminApp into user_console_v2
    # Conflicts:
    #	src/store/actions/actions.js
    
    Files Changed:
    actions.js
    workspace.xml

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -16,7 +16,7 @@ export const { getUserData } = createUserAction
 
 //------------------------------Console Action-------------------------------//
 
-export const { getTableColumnData, getConsoleUserData } = consoleActions
+export const { getTableColumnData, getConsoleUserData, commonConsoleAction } = consoleActions
 
 //------------------------------xxxxxxxxxxxxxx-------------------------------//
 
